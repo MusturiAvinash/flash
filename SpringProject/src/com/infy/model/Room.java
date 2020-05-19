e0e3b1c9e50208f2004b5b515a8bb79d01eeb0f2
MusturiAvinash <59835841+MusturiAvinash@users.noreply.github.com>
2020-05-19 19:04:28 +0530

    First commit uploaded all files

---
+++ SpringProject/src/com/infy/model/Room.java
@@ -0,0 +1,21 @@
+package com.infy.model;
+
+public class Room {
+  private int roomNumber;
+  private Student allotedTo;
+  
+public int getRoomNumber() {
+	return roomNumber;
+}
+public void setRoomNumber(int roomNumber) {
+	this.roomNumber = roomNumber;
+}
+public Student getAllotedTo() {
+	return allotedTo;
+}
+public void setAllotedTo(Student allotedTo) {
+	this.allotedTo = allotedTo;
+}
+  
+  
+}
